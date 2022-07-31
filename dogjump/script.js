c2b0b63df0ed42710477291afa58ab03a604272b
LuYee6813 <aulaul920106@gmail.com>
2022-07-31 15:14:30 +0800

    update

---
+++ dogjump/script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playimage = new Image();
+playimage.src = ('assets/shadow_dog.png');
+
