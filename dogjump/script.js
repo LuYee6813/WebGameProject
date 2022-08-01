4517038565b5900f2f24ef23ac7aa2987c4a3379
LuYee6813 <aulaul920106@gmail.com>
2022-08-01 19:32:50 +0800

    update

--- dogjump/script.js
+++ dogjump/script.js
@@ -1,17 +1,24 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 300;
-const CANVAS_HEIGHT = canvas.height = 300;
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
 
-console.log(ctx);
+
 
 const playerImage = new Image();
 playerImage.src = ('assets/shadow_dog.png');
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 0;
+
 
 function animate(){
-    ctx.clearReact(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.fillRect(100,50,100,100);
-    ctx.drawImage(playerImage, 0 , 0);
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     requestAnimationFrame(animate);
 };
 animate();
